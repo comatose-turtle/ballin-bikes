ee01c28c5207ece8fb74e3f4f8a6ce10ab040d10
Thomas Orr <comatose_turtle@hotmail.com>
2014-09-08 17:33:41 -0700

    Rails app initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BallinBikes::Application.config.secret_token = '5b2cca1427cdf1e8c1ab200017455be5d900590902617962eac0bd18dff80860eb05c49b8e1ab23d769465df9572e467f2ad1324593aac265bfa8fa785f54772'
